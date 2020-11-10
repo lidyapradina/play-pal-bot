1842c3f5a79d1a98c85b46fb60d150039a5c13c1
lidyapradina <67100523+lidyapradina@users.noreply.github.com>
2020-11-10 15:00:29 +0700

    Create webhook.js

---
+++ webhook.js
@@ -0,0 +1,12 @@
+"name": "test webhook",
+  "type": 1,
+  "channel_id": "199737254929760256",
+  "token": "NzY4MjkxNTIxMzUwNzI5NzMx.X4-VPA.PJLk7TZHcTVtIxOg4kY73wCxD6s,"
+  "avatar": null,
+  "guild_id": "199737254929760256",
+  "id": "223704706495545344",
+  "user": {
+    "username": "playpal",
+    "discriminator": "7479",
+    "id": "768291521350729731",
+    "avatar": "b004ec1740a63ca06ae2e14c5cee11f3"
